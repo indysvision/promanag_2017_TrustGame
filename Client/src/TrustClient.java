3b03bbe9b2c3f1cf2cd56a9557ce129e1e3a070e
popea <andreiovidiupopescu@yahoo.com>
2017-11-10 10:27:15 +0200

    simple chat client server

--- Client/src/TrustClient.java
+++ Client/src/TrustClient.java
@@ -1,8 +1,11 @@
+import communication.ChatClient;
 
 public class TrustClient {
-
+	public static final int DEFAULT_PORT = 5892;
+	public static final String LOCALHOST = "localhost";
+	
 	public static void main(String[] args) {
-
+		ChatClient client = new ChatClient(LOCALHOST, DEFAULT_PORT);
 	}
 
 }
--- Server/src/TrustServer.java
+++ Server/src/TrustServer.java
@@ -1,8 +1,10 @@
+import communication.ChatServer;
 
 public class TrustServer {
+	public static final int DEFAULT_PORT = 5892;
 
 	public static void main(String[] args) {
-
+		ChatServer server = new ChatServer(DEFAULT_PORT);
 	}
 
 }
